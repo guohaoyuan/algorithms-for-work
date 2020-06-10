93f59a104f6a18c57dd174ebcd43308b6e3fa6d6
guohaoyuan <970992936@qq.com>
2020-06-10 11:35:31 +0800

    完善改进更清晰

--- leetcode/树/236. 二叉树的最近公共祖先/lowestCommonAncestor.py
+++ leetcode/树/236. 二叉树的最近公共祖先/lowestCommonAncestor.py
@@ -2,21 +2,29 @@
 
 class Solution:
     def lowestCommonAncestor(self, root, p, q):
-        # 1. 递归结束条件：越过叶子节点，直接返回
+        """
+        分四种情况：
+        1. 根节点==其中一个值，则返回分解点
+        2. 左子树为空，则在右子树
+        3. 右子树为空，则在左子树
+        4. 左右子树均为存在，则在根节点
+        :param root:
+        :param p:
+        :param q:
+        :return:
+        """
         if not root:
             return
 
-        # 2. 递归操作：
+        if root.val == q.val or root.val == p.val:
+            return root
+
         l = self.lowestCommonAncestor(root.left, p, q)
         r = self.lowestCommonAncestor(root.right, p, q)
 
-        # 分三种情况
-        if root.val == p.val or root.val == q.val:
-            return root
+        if not l and r:
+            return r
+        if not r and l:
+            return l
         if l and r:
             return root
-
-        if l and not r:
-            return l
-        if r and not l:
-            return r
