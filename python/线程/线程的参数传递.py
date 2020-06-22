0b3245f0727c18eacef4d82d67ad20290ab40ffa
guohaoyuan <970992936@qq.com>
2020-06-22 21:15:53 +0800

    线程传参

---
+++ python/线程/线程的参数传递.py
@@ -0,0 +1,25 @@
+import threading
+
+"""
+在线程中传递参数有三种方法, 
+1. 使用元组传递threading.Thread(target=xx, args=(参数1, 参数2,...))
+2. 使用字典传递threading.Thread(target=xx, kwargs={})
+3. 同时使用元组和字典传参
+"""
+def sing(a, b, c):
+    print("参数 a %d, b %s, c %s" % (a, b, c))
+    for i in range(5):
+        print("singing")
+
+
+def dance(a, b, c):
+    print("参数 a %d, b %s, c %s" % (a, b, c))
+    for i in range(5):
+        print("dancing")
+
+
+if __name__ == "__main__":
+    threading_obj1 = threading.Thread(target=sing, args=(1, 2, 3))
+    threading_obj2 = threading.Thread(target=dance, kwargs={"a": 1, "b": 2, "c": 3})
+    threading_obj1.start()
+    threading_obj2.start()
