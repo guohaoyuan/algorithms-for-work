706b9e44c832af9ceb7e6186940c6a8fe6f2aa94
guohaoyuan <970992936@qq.com>
2020-06-30 17:08:52 +0800

    快慢指针的判断条件的批注

--- leetcode/双指针/快慢指针/141. 环形链表/hasCycle.py
+++ leetcode/双指针/快慢指针/141. 环形链表/hasCycle.py
@@ -13,7 +13,8 @@ class Solution:
         slow, fast = head, head
 
         while True:
-            # 如果遇到结尾就跳出
+            # 如果遇到结尾就跳出, 因为快慢指针,分奇数偶数节点情况, 所以需要not fast or not fast.next,
+            # 遇到快慢指针, 这样做判断就是对的
             if not fast or not fast.next:
                 return False
             else:
