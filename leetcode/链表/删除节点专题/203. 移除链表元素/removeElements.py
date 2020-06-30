45d309624ae5e191f1eef93a94d4d0d89b9af6fb
guohaoyuan <970992936@qq.com>
2020-06-30 17:58:55 +0800

    本身就是不断找最适合自己的方法,加油,冲鸭

--- leetcode/链表/删除节点专题/203. 移除链表元素/removeElements.py
+++ leetcode/链表/删除节点专题/203. 移除链表元素/removeElements.py
@@ -34,3 +34,47 @@ class Solution:
                     cur = cur.next
                 else:   #  要删除的值不存在
                     return head
+
+
+
+"""
+下面的方法可以将返回值集中在一个条件中哦
+比较方便
+"""
+
+
+class Solution1:
+    def deleteNode(self, head: ListNode, val: int) -> ListNode:
+        if not head:
+            return head
+
+        # 对于不是尾部的节点使用复制删除法,对于尾部节点就需要使用pre和cur指针
+        # 由于头节点有可能使被删除的对象
+        pre = None
+        cur = head
+
+        while True:
+            if cur.next:
+                # 不是tail
+                if cur.val == val:
+                    # 是删除节点,使用复制删除法
+                    cur.val = cur.next.val
+                    cur.next = cur.next.next
+                else:
+                    # 不是删除节点
+                    pre = cur
+                    cur = cur.next
+            else:
+                # 是tail
+                if cur.val == val:
+                    # 是删除节点
+                    if not pre:
+                        # 长度为1,
+                        return pre
+                    else:
+                        # 长度不为1
+                        pre.next = None
+                        return head
+                else:
+                    # tail不是删除节点
+                    return head
