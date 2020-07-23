15487670f460399c957014fa939f9aedff90e483
guohaoyuan <970992936@qq.com>
2020-07-23 20:26:18 +0800

    凡人之躯比肩神明

---
+++ leetcode/高频面试/动态规划/300. 最长上升子序列 medium 没有思路/lengthOfLIS.py
@@ -0,0 +1,66 @@
+"""
+
+定义状态：
+        dp[i]表示以nums[i]结尾的数组上升子序列长度为dp[i]
+
+状态转移方程：
+        dp[i] = max(dp[i], dp[j] + 1) ， j in range(i)
+
+base case:
+        dp[0] = 1
+
+步骤：
+    1. 特殊情况：数组为空，直接返回0
+    2.
+"""
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # 特殊情况：数组为空
+        if not nums:
+            return 0
+
+        n = len(nums)
+
+        dp = [1 for _ in range(n)]
+
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+
+"""
+利用二分法
+有点类似打牌，创建一个空数组res，如果res最后一个元素<当前元素，我们将其入res;
+                                否则，我们在牌堆中为其找一个合适的位置，替换掉那个位置的元素，
+最后的到的res长度就是最长上升子序列的长度
+"""
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        res = []
+
+        for num in nums:
+            if not res or res[-1] < num:
+                res.append(num)
+            else:
+                L, R = 0, len(res) - 1
+                while L <= R:
+                    mid = (L + R) // 2
+                    if L == R:
+                        res[L] = num
+                    if res[mid] > num:
+                        # 搜索区间在[L, mid]
+                        R = mid
+                    elif res[mid] < num:
+                        # 搜索区间在[mid + 1, R]
+                        L = mid + 1
+                    elif res[mid] == num:
+                        break
+        return len(res)
