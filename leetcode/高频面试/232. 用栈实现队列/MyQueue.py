1fd1535db599348471affddec74e84ceac413c4f
guohaoyuan <970992936@qq.com>
2020-07-24 17:06:52 +0800

    冲鸭，重重重，浴火重生

--- leetcode/高频面试/232. 用栈实现队列/MyQueue.py
+++ leetcode/高频面试/232. 用栈实现队列/MyQueue.py
@@ -66,3 +66,59 @@ class MyQueue:
 # param_2 = obj.pop()
 # param_3 = obj.peek()
 # param_4 = obj.empty()
+
+"""
+不得不承认，labuladong的代码很精练
+"""
+
+
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.data = []
+        self.helper = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.data.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        if not self.helper:
+            while self.data:
+                self.helper.append(self.data.pop())
+        tmp = self.helper.pop()
+        return tmp
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        if not self.helper:
+            while self.data:
+                self.helper.append(self.data.pop())
+        tmp = self.helper[-1]
+        return tmp
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        if not self.helper and not self.data:
+            return True
+        else:
+            return False
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
