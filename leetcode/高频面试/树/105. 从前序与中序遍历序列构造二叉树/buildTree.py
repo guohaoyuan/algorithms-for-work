421f19cc0d13682f5819fd17e7748cce011f6d00
guohaoyuan <970992936@qq.com>
2020-07-27 09:49:28 +0800

    适合自己的才是最好的

--- leetcode/高频面试/树/105. 从前序与中序遍历序列构造二叉树/buildTree.py
+++ leetcode/高频面试/树/105. 从前序与中序遍历序列构造二叉树/buildTree.py
@@ -33,3 +33,34 @@ class Solution:
                 root.right = self.buildTree(preorder[i + 1:], inorder[i + 1:])
                 # break   ### 备忘
         return root
+
+"""
+自己的写法，很适合我自己
+加油，冲冲冲
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        # 递归结束条件，数组为空
+        if not preorder:
+            return None
+
+        # 初始化根节点
+        root = TreeNode(preorder[0])
+        # 寻找分割点
+        for i, val in enumerate(inorder):
+            if val == root.val:
+                root_index = i
+        # 对于inorder左子树[:root_index];右子树[root_index + 1:]
+        # 对于preorder左子树[1:1+root_index];右子树[1+root_index:]
+        root.left = self.buildTree(preorder[1:1 + root_index], inorder[:root_index])
+        root.right = self.buildTree(preorder[root_index + 1:], inorder[1 + root_index:])
+        return root
