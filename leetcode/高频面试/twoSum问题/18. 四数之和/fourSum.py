df0a7e50061d2242e3532238996457c0c1e128a8
guohaoyuan <970992936@qq.com>
2020-07-30 22:59:27 +0800

    不太会

--- leetcode/高频面试/twoSum问题/18. 四数之和/fourSum.py
+++ leetcode/高频面试/twoSum问题/18. 四数之和/fourSum.py
@@ -13,46 +13,46 @@ for i in range(n-3):    # 需要留出三个位置给后三个数
         if j > 0 and nums[j] == nums[j-1]: continue
 
         利用二分法遍历数组
-        while L <= R:
+        while L < R:    #
             四数之和等于目标，添加至结果。另外要避免重复，如果L++后对应的数与前一位相等，则需要继续后移。（所以说多了重复操作就在这）如果R--前的数与后一位相等，则需要继续前移。
             四数之和小于目标，L++
             四数之和大于目标，R--
 
 """
 
-class Solution:
 
+class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
         n = len(nums)
 
-        if n < 4:
-            return []
+        if n <= 3:
+            return res
 
-        res = []
         nums.sort()
 
         for i in range(n - 3):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-
             for j in range(i + 1, n - 2):
                 if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
 
                 L, R = j + 1, n - 1
+                while L <= R:
+                    if L == R:
+                        break
 
-                while L < R:
-                    if nums[i] + nums[j] + nums[L] + nums[R] == target:
+                    if nums[L] + nums[R] + nums[i] + nums[j] == target:
                         res.append([nums[i], nums[j], nums[L], nums[R]])
                         while L < R and nums[L] == nums[L + 1]:
                             L += 1
                         while L < R and nums[R] == nums[R - 1]:
                             R -= 1
-
                         L += 1
                         R -= 1
-                    elif nums[i] + nums[j] + nums[L] + nums[R] > target:
-                        R -= 1
-                    elif nums[i] + nums[j] + nums[L] + nums[R] < target:
+                    elif nums[L] + nums[R] + nums[i] + nums[j] < target:
                         L += 1
+                    elif nums[L] + nums[R] + nums[i] + nums[j] > target:
+                        R -= 1
         return res
