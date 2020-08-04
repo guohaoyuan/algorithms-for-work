d83a4d738c758d2d829c116dc9a365f102012de7
guohaoyuan <970992936@qq.com>
2020-08-04 20:42:30 +0800

    祝我好运

--- copy_ex.py
+++ copy_ex.py
@@ -1,22 +1,44 @@
 # -*- coding = utf-8 -*-
 
 
-class Animal(object):
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Animal(object):
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def eat(self):
+#         print("%s is eating!" % self.name)
+#
+# class Person(Animal):
+#     def __init__(self, name, age, sex):
+#         super(Person, self).__init__(name, age)
+#         self.sex = sex
+#
+#     def eat(self):
+#         super(Person, self).eat()
+#         print("person is eating!")
+#
+# person = Person("ghy", 35, "nan")
+# person.eat()
 
-    def eat(self):
-        print("%s is eating!" % self.name)
+import sys
 
-class Person(Animal):
-    def __init__(self, name, age, sex):
-        super(Person, self).__init__(name, age)
-        self.sex = sex
+input_data = []
+data = []
 
-    def eat(self):
-        super(Person, self).eat()
-        print("person is eating!")
+for line in sys.stdin:
+    input_data = line.split()
+    data.append([int(x) for x in input_data])
+print(data)
 
-person = Person("ghy", 35, "nan")
-person.eat()
+def helper(target, nums, coins):
+    res = 0
+
+    for i in range(len(nums) - 1, -1, -1):
+        need = min(target//coins[i], nums[i])
+        target = target - need*coins[i]
+        res += need
+        if target == 0:
+            return res
+
+print(helper(data[1], data[0], [1, 3, 7, 11, 13]))
---
+++ java3/src/基本数据类型/addIntDemo.java
@@ -0,0 +1,8 @@
+package 基本数据类型;
+
+public class addIntDemo {
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE + 1 == Integer.MIN_VALUE);
+        System.out.println(Integer.MIN_VALUE - 1 == Integer.MAX_VALUE);
+    }
+}
