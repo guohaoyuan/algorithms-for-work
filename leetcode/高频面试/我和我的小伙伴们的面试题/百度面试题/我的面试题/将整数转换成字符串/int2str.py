824f1bca2a35f8e645069f963aac4d80faea4979
guohaoyuan <970992936@qq.com>
2020-08-09 22:36:01 +0800

    重复第三遍了，至少

--- leetcode/高频面试/我和我的小伙伴们的面试题/百度面试题/我的面试题/将整数转换成字符串/int2str.py
+++ leetcode/高频面试/我和我的小伙伴们的面试题/百度面试题/我的面试题/将整数转换成字符串/int2str.py
@@ -41,8 +41,22 @@ class Solution:
                 break
         res = res[::-1]
         return res
-
+def num2str(num):
+    res = ""
+    while True:
+        shang, yu = num // 3, num % 3
+        if yu == 1:
+            res += 'a'
+        elif yu == 2:
+            res += 'b'
+        elif yu == 0:
+            res += 'c'
+            shang = shang - 1
+        num = shang
+        if shang == 0:
+            return res[::-1]
 if __name__ == '__main__':
     test1 = 18
     solution = Solution()
     print(solution.int2str(test1))
+    print(num2str(test1))
