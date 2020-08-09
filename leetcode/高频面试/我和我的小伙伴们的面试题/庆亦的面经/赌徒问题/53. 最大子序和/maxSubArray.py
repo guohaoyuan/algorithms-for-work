5c9d59b21ab41e7d88a14199166a1c0ea4723413
guohaoyuan <970992936@qq.com>
2020-08-09 21:01:14 +0800

    重复第三遍了，至少

--- leetcode/高频面试/我和我的小伙伴们的面试题/庆亦的面经/赌徒问题/53. 最大子序和/maxSubArray.py
+++ leetcode/高频面试/我和我的小伙伴们的面试题/庆亦的面经/赌徒问题/53. 最大子序和/maxSubArray.py
@@ -3,7 +3,7 @@
 2. 初始化一个数组，用于存放当前位置的最大数字res = [0] * len(nums), res[0] = nums[0]
 3. for i in range(1, n):
         if res[i-1] + nums[i] > 0:
-            res[i] = max(res[i-1] + nums[i], nums[i])
+            res[i] = res[i-1] + nums[i]
         elif:
             res[i] = nums[i]
 4. return max(res)
