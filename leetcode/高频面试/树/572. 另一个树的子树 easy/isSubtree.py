17ae8f8790aa6a5c431c1c3d21f95d7161d9e2fe
guohaoyuan <970992936@qq.com>
2020-08-18 23:09:21 +0800

    这个题是真的记不住

--- leetcode/高频面试/树/572. 另一个树的子树 easy/isSubtree.py
+++ leetcode/高频面试/树/572. 另一个树的子树 easy/isSubtree.py
@@ -13,3 +13,27 @@
     2. 都为空，就就可以
     3. 如果两颗树都存在，则返回两颗树相同或者是左子树或者是右子树
 """
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        if not s and t:
+            return False
+        if not t and s:
+            return False
+        if not s and not t:
+            return True
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t) or self.isSame(s, t)
+    def isSame(self, s, t):
+        if not s and t:
+            return False
+        if not t and s:
+            return False
+        if not t and not s:
+            return True
+        return s.val == t.val and self.isSame(s.left, t.left) and self.isSame(s.right, t.right)
