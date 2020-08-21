87343bf7dcdb9ec707a59739b064963570536816
guohaoyuan <970992936@qq.com>
2020-08-21 08:10:37 +0800

    leetcode572

--- leetcode/高频面试/树/572. 另一个树的子树 easy/isSubtree.py
+++ leetcode/高频面试/树/572. 另一个树的子树 easy/isSubtree.py
@@ -37,3 +37,44 @@ class Solution:
         if not t and not s:
             return True
         return s.val == t.val and self.isSame(s.left, t.left) and self.isSame(s.right, t.right)
+
+"""
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        # 当前节点出发，就是看两树是否一样
+        # 当前节点的右子树出发，就是看是否是右子树
+        # 当前节点的左子树出发，就是看是否是左子树
+
+        if not s and not t:
+            # 两个树都不存在，肯定是满足条件的
+            return True
+
+        if not s or not t:
+            # 两个树有一个不存在，肯定是不行的
+            return False
+
+        # 接下来肯定是两个树都存在的情况
+        return self.isSame(s, t) or self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+
+    def isSame(self, s, t):
+        # 如果两个树都不存在，是不满足条件的
+        if not s and not t:
+            return True
+
+        # 如果有一个树存在
+        if not s or not t:
+            return False
+
+        # 都存在
+        # 左右子树和当前节点都想等
+
+        return s.val == t.val and self.isSame(s.left, t.left) and self.isSame(s.right, t.right)
