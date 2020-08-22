d1f5787de7e28430331dc42068b2ae120a5bcc29
guohaoyuan <970992936@qq.com>
2020-08-22 21:47:09 +0800

    leetcode 112

--- leetcode/高频面试/树/112. 路径总和 easy 简单题也不会 要重视/hasPathSum.py
+++ leetcode/高频面试/树/112. 路径总和 easy 简单题也不会 要重视/hasPathSum.py
@@ -24,3 +24,30 @@ class Solution:
 
         return self.hasPathSum(root.left, sum - root.val) or \
                self.hasPathSum(root.right, sum - root.val)
+
+
+"""
+做了一个简单的封装
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        def helper(root, sum):
+            if not root:
+                return False
+
+            if not root.left and not root.right and sum - root.val == 0:
+                return True
+
+            return helper(root.left, sum - root.val) or helper(root.right, sum - root.val)
+
+        res = helper(root, sum)
+        return res
