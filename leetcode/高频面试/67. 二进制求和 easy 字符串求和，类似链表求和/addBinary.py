ffd4c00790003ab092e22925981bc88ce5c2cec5
guohaoyuan <43543066+guohaoyuan@users.noreply.github.com>
2021-07-29 11:54:33 +0800

    Update addBinary.py

--- leetcode/高频面试/67. 二进制求和 easy 字符串求和，类似链表求和/addBinary.py
+++ leetcode/高频面试/67. 二进制求和 easy 字符串求和，类似链表求和/addBinary.py
@@ -1,42 +1,50 @@
-class Solution:
-    def addBinary(self, a: str, b: str) -> str:
-        n1 = len(a)
-        n2 = len(b)
-        # 特殊情况
-        if not a:
-            return b
-        if not b:
-            return a
-
-        # 表示进位符号
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        # 1. 初始化两个字符串的长度n1 n2;两个字符串的指针p1, p2,；进位符号tmp
+        n1, n2 = len(a), len(b)
+        p1, p2 = n1 - 1, n2 - 1
         tmp = 0
-        # 存储结果
         res = ""
-        # 相加和
-        while n1 and n2:
-            # 两数之和加上进位符
-            sum_ = int(a[-1]) + int(b[-1]) + int(tmp)
+        sum_ = 0
+
+        # 2. 从后往前一次相加
+        while p1 >= 0 and p2 >= 0:
+            sum_ = int(a[p1]) + int(b[p2]) + tmp
             shang, yu = sum_ // 2, sum_ % 2
-            res += str(yu)
-            # 更新字符串和进位符
-            tmp = shang
-            a, b = a[:-1], b[:-1]
-            n1 -= 1
-            n2 -= 1
-        while n1:
-            sum_ = int(a[-1]) + tmp
-            shang, yu = sum_ // 2, sum_ % 2
+
+            # 更新结果res和进位符号
             res += str(yu)
             tmp = shang
-            a = a[:-1]
-            n1 -= 1
-        while n2:
-            sum_ = int(b[-1]) + tmp
+
+            # 更新两个指针
+            p1 -= 1
+            p2 -= 1
+        
+        while p1 >= 0:
+            sum_ = int(a[p1]) + tmp
             shang, yu = sum_ // 2, sum_ % 2
+
             res += str(yu)
             tmp = shang
-            b = b[:-1]
-            n2 -=1
-        if tmp:
-            res += str(1)
-        return res[::-1]
+
+            p1 -= 1
+            
+        while p2 >= 0:
+            sum_ = int(b[p2]) + tmp
+            shang, yu = sum_ //2, sum_ % 2
+
+            res += str(yu)
+            tmp = shang
+
+            p2 -= 1
+        
+        # 3. 对最后的进位符号进行处理
+        if tmp > 0:
+            res += str(tmp)
+        res = res[::-1]
+        return res
