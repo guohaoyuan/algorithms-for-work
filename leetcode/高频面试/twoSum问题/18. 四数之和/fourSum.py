bea0478042dcb2c0354b3ea263d02676dcebf6e9
guohaoyuan <43543066+guohaoyuan@users.noreply.github.com>
2021-08-02 18:58:40 +0800

    Update fourSum.py

--- leetcode/高频面试/twoSum问题/18. 四数之和/fourSum.py
+++ leetcode/高频面试/twoSum问题/18. 四数之和/fourSum.py
@@ -56,3 +56,68 @@ class Solution:
                     elif nums[L] + nums[R] + nums[i] + nums[j] > target:
                         R -= 1
         return res
+    """
+1. 初始化数组的长度n，排除特殊情况n<4的
+2. 对数组进行排序
+3. 第一层遍历，遍历数组，并初始化元素a=nums[i]
+    4. 跳过a重复的情况，nums[i]=nums[i-1]
+    5. 第二层遍历,找到b=nums[j], j = i +1，遍历区间不考虑最后两个元素，因为还有c d 
+        6. 初始化两个指针L=j+1 R=n-1 ， c= L d=R
+        7. 双指针进行遍历，记得排除元素
+"""
+"""
+1. 初始化数组的长度n，排除特殊情况n<4的
+2. 对数组进行排序
+3. 第一层遍历，遍历数组，并初始化元素a=nums[i]
+    4. 跳过a重复的情况，nums[i]=nums[i-1]
+    5. 第二层遍历,找到b=nums[j], j = i +1，遍历区间不考虑最后两个元素，因为还有c d 
+        6. 初始化两个指针L=j+1 R=n-1 ， c= L d=R
+        7. 双指针进行遍历，记得排除元素
+"""
+
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+
+        res = []
+        n = len(nums)
+        if n < 4:
+            return res
+        
+        nums.sort()
+
+        for i in range(n):
+            a = nums[i]
+
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            
+            for j in range(i + 1, n - 2):
+                b = nums[j]
+
+                if j > i+1 and nums[j] == nums[j-1]:
+                    continue
+                L, R = j + 1, n - 1
+                while L < R:
+                    c, d = nums[L], nums[R]
+                    sum_= a + b + c + d
+                    if sum_ == target:
+                        res.append([a, b, c, d])
+                        while L < R and nums[L] == nums[L + 1]:
+                            L += 1
+                        while L < R and nums[R] == nums[R - 1]:
+                            R -= 1
+                        
+                        L += 1
+                        R -= 1
+                    elif sum_ > target:
+                        R -= 1
+                    else:
+                        L += 1
+        return res
+
