b35f3f1d9044912139a78971d3e54eabd8f25537
guohaoyuan <43543066+guohaoyuan@users.noreply.github.com>
2021-08-02 17:22:25 +0800

    Update twoSum.py

--- leetcode/高频面试/twoSum问题/167. 两数之和 II - 输入有序数组/twoSum.py
+++ leetcode/高频面试/twoSum问题/167. 两数之和 II - 输入有序数组/twoSum.py
@@ -1,29 +1,45 @@
-class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # 1. 特殊情况
-        n = len(numbers)
+"""
+按照三数之和去做
+"""
 
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # 1. 初始化长度和排除长度不足2的情况
+        n = len(numbers)
         if n < 2:
             return []
+        
+        # 不用排序了,已经是升序了
 
-        res = []
-
+        # 2. 遍历数组nums，并初始化元素a
         for i in range(n):
-            if i > 0 and numbers[i] == numbers[i - 1]:
+            a = numbers[i]
+
+            # 3. 排除重复元素
+            if i > 0 and numbers[i] == numbers[i-1]:
                 continue
-            L = 0
-            R = n - 1
-            while L < R:
-                if numbers[L] + numbers[R] == target:
-                    return [L + 1, R + 1]
-                    # while L < R and numbers[L] == numbers[L + 1]:
-                    #     L += 1
-                    # while L < R and numbers[R] == numbers[R - 1]:
-                    #     R -= 1
-                    # L += 1
-                    # R -= 1
-                elif numbers[L] + numbers[R] < target:
-                    L += 1
-                elif numbers[L] + numbers[R] > target:
-                    R -= 1
-        return res
+
+            # 4. 利用双指针进行遍历
+            j = n - 1
+            while i < j:
+                b = numbers[j]
+                sum_ = a + b
+
+                if sum_ == target:
+                    # 直接返回结果了
+                    return [i + 1, j + 1]
+                    # res.append([i + 1, j + 1])
+                    # 5. 更新b
+                    # while numbers[j] == numbers[j+1]:
+                    #     j -= 1
+                    # j -= 1
+                    
+                elif sum_ > target:
+                    j -= 1
+                else:
+                    i += 1
