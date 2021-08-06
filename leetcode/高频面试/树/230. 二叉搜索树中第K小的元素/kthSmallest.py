1ada51687209e2343e1b56b9b79728774fbcda08
guohaoyuan <43543066+guohaoyuan@users.noreply.github.com>
2021-08-06 15:24:28 +0800

    Update kthSmallest.py

--- leetcode/高频面试/树/230. 二叉搜索树中第K小的元素/kthSmallest.py
+++ leetcode/高频面试/树/230. 二叉搜索树中第K小的元素/kthSmallest.py
@@ -1,29 +1,45 @@
+"""
+二叉搜索树是有序的结构
+一般“什么时候做的问题”是在中序遍历
+“怎么做问题”是建立一个数count用来计数，如果当前位置是第k个位置，就可以返回结果了
+"""
+
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def kthSmallest(self, root: TreeNode, k: int) -> int:
+class Solution(object):
 
-        self.res = 0
+    def __init__(self):
+        # 用于计数和返回结果
         self.count = 0
+        self.res = -1
 
-        # 定义中序遍历
-        def helper(root, k):
-            if not root:
-                return
-
-            helper(root.left, k)
-            # self.res.append(root.val)
-            self.count += 1
-            if self.count == k:
-                self.res = root.val
-            helper(root.right, k)
-
+    def inorder(self, root, k):
+        # 特殊情况：当前节点为空
         if not root:
             return
+        
+        self.inorder(root.left, k)
 
-        helper(root, k)
+        # 计数
+        self.count += 1
+        # 确认是不是第k次
+        if k == self.count:
+            self.res = root.val
+        
+        self.inorder(root.right, k)
+
+        return
+
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        self.inorder(root, k)
+
         return self.res
